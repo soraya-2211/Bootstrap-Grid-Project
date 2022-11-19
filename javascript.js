646a5c1ed81b42c2f561970618499009c69e50c9
Soraya <117850430+soraya-2211@users.noreply.github.com>
2022-11-19 10:18:55 -0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,37 @@
+const emails = ["Email-1", "Email-2", "Email-3", "Email-4"];
+
+function ShowAndHide1() {
+    var x = document.getElementById('Email-1');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function ShowAndHide2() {
+    var x = document.getElementById('Email-2');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function ShowAndHide3() {
+    var y = document.getElementById('Email-3');
+    if (y.style.display == 'none') {
+        y.style.display = 'block';
+    } else {
+        y.style.display = 'none';
+    }
+}
+
+function ShowAndHide4() {
+    var y = document.getElementById('Email-4');
+    if (y.style.display == 'none') {
+        y.style.display = 'block';
+    } else {
+        y.style.display = 'none';
+    }
+}
